994ded6de90a3982812026e4de1873a6298d6fcd
zjr <zach@glumptio.us>
2012-08-02 21:23:59 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NMikaco::Application.config.secret_token = 'bfcc2cbb1a5f02fadce866b6a23930c87eb2e4df5983427e8e4921cd75bd431e560cc54b55c146ebcb3cfdd5d94c13fafe26ba6e02cdf3c5ef492e9ff6001259'
