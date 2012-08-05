44b5849bf652fa0f7109a0954d37176d7d872fe6
zjr <zach@glumptio.us>
2012-08-05 01:21:54 -0700

    added fonts; prefix-free; styling work

--- app/overrides/spree_app_or.rb
+++ app/overrides/spree_app_or.rb
@@ -5,3 +5,7 @@ Deface::Override.new(:virtual_path => "spree/shared/_taxonomies",
                      :name => "h6_taxonomy_root",
                      :replace => "code[erb-loud]:contains('t(:shop_by_taxonomy, :taxonomy => taxonomy.name.singularize)')",
                      :text => "<%= t(:shop_by_taxonomy, :taxonomy => taxonomy.name) %>")
+Deface::Override.new(:virtual_path => "spree/layouts/spree_application", 
+                     :name => "logo", 
+                     :replace_contents => "#logo", 
+                     :text => "<h2><a class=\"brand\" href=\"/\" alt=\"Mika Couture Home\">Mika Couture</a></h2>")
